f8ddb80e36d2e3ec1f6f1cf2230856e3960d5e14
beiju <beiju@me.com>
2025-04-22 17:49:55 -0700

    Expire cache after 5 minutes
    Doesn't delete the files though

--- basic_stats.py
+++ basic_stats.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from pathlib import Path
+from datetime import datetime, timedelta, timezone
 
 import requests
 
@@ -21,11 +22,18 @@ def get_json(url: str) -> dict:
     except FileNotFoundError:
         pass
 
-    if url in cache:
+    # Return from cache if the cache entry is less than 5 minutes old
+    now = datetime.now(timezone.utc)
+    if (
+            url in cache and
+            "__archived_at" in cache[url] and
+            cache[url]["__archived_at"] > (now - timedelta(minutes=5)).isoformat()
+    ):
         return cache[url]
 
     data = requests.get(url).json()
     cache[url] = data
+    cache[url]["__archived_at"] = now.isoformat()
     with open(cache_file_path, "w") as cache_file:
         json.dump(cache, cache_file)
 
@@ -37,51 +45,55 @@ def main():
     for player in team_obj["Players"]:
         player_obj = get_json(f"https://mmolb.com/api/player/{player['PlayerID']}")
 
-        if not player_obj["Stats"]:
+        try:
+            # I'm pretty sure IDs are lexicographically ordered, so we want the
+            # maximum value to get stats for the latest season
+            stats_obj = player_obj["Stats"][max(player_obj["Stats"].keys())]
+        except ValueError:
             print("No stats for", player["FirstName"], player["LastName"])
+            continue
 
-        for stats_obj in player_obj["Stats"].values():
-            singles = stats_obj.get("singles", 0)
-            doubles = stats_obj.get("doubles", 0)
-            triples = stats_obj.get("triples", 0)
-            home_runs = stats_obj.get("home_runs", 0)
-            hits = singles + doubles + triples + home_runs
-            bb = stats_obj.get("walked", 0)
-            hbp = 0  # I need an example of this to see what the key is
-            earned_runs = stats_obj.get("earned_runs", 0)
+        singles = stats_obj.get("singles", 0)
+        doubles = stats_obj.get("doubles", 0)
+        triples = stats_obj.get("triples", 0)
+        home_runs = stats_obj.get("home_runs", 0)
+        hits = singles + doubles + triples + home_runs
+        bb = stats_obj.get("walked", 0)
+        hbp = 0  # I need an example of this to see what the key is
+        earned_runs = stats_obj.get("earned_runs", 0)
 
-            try:
-                ab = stats_obj["at_bats"]
-            except KeyError:
-                ba_str = None
-            else:
-                ba = hits / ab
-                ba_str = f"BA: {ba:.3f} ({ab} AB)"
+        try:
+            ab = stats_obj["at_bats"]
+        except KeyError:
+            ba_str = None
+        else:
+            ba = hits / ab
+            ba_str = f"BA: {ba:.3f} ({ab} AB)"
 
-            try:
-                pa = stats_obj["plate_appearances"]
-                ab = stats_obj["at_bats"]
-            except KeyError:
-                ops_str = None
-            else:
-                obp = (hits + bb + hbp) / pa
-                slg = (singles + 2 * doubles + 3 * triples + 4 * home_runs) / ab
-                ops = obp + slg
-                pa_str = dot_format(pa)
-                ops_str = f"OBP: {obp:.3f}, SLG: {slg:.3f}, OPS: {ops:.3f} ({pa_str} PA)"
+        try:
+            pa = stats_obj["plate_appearances"]
+            ab = stats_obj["at_bats"]
+        except KeyError:
+            ops_str = None
+        else:
+            obp = (hits + bb + hbp) / pa
+            slg = (singles + 2 * doubles + 3 * triples + 4 * home_runs) / ab
+            ops = obp + slg
+            pa_str = dot_format(pa)
+            ops_str = f"OBP: {obp:.3f}, SLG: {slg:.3f}, OPS: {ops:.3f} ({pa_str} PA)"
 
-            try:
-                ip = stats_obj["batters_faced"] / 3
-            except KeyError:
-                era_str = None
-            else:
-                era = 9 * earned_runs / ip
-                ip_str = dot_format(ip)
-                era_str = f"ERA {era:.2f} ({ip_str} IP)"
+        try:
+            ip = stats_obj["batters_faced"] / 3
+        except KeyError:
+            era_str = None
+        else:
+            era = 9 * earned_runs / ip
+            ip_str = dot_format(ip)
+            era_str = f"ERA {era:.2f} ({ip_str} IP)"
 
-            stats_str = ", ".join(s for s in [ba_str, ops_str, era_str] if s is not None)
-            if stats_str:
-                print(player["Position"], player["FirstName"], player["LastName"], stats_str)
+        stats_str = ", ".join(s for s in [ba_str, ops_str, era_str] if s is not None)
+        if stats_str:
+            print(player["Position"], player["FirstName"], player["LastName"], stats_str)
 
 
 def dot_format(in_val: float) -> str:
